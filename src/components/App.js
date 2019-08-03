a1ade3d18fe236269b246214e4ed5848936b4353
metas-mk <mark.krake@metasfresh.com>
2019-08-03 19:55:36 +0200

    Refactor request to async/ await

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    // promise
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID YOUR_ACCESS_KEY'
       }
+      //'then' waits for respond and invokes the callback funtion when done
     });
+    console.log(response.data.results);
   }
 
   render() {
