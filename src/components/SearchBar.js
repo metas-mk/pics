4349474ea6cc2cce282f138800adab89a65ac243
metas-mk <mark.krake@metasfresh.com>
2019-08-03 13:42:26 +0200

    Preventing default HTML Form refresh onSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
   state = { term: '' };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <input
               type="text"
